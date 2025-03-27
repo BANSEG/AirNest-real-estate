16aee01282082f134b98f750a3e20e3e34acd1c3
Banseg <bankoleoluwasegun17@yahoo.com>
2025-03-27 13:58:08 +0100

    new components

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./Pages/home.jsx";
-import EstateDetails from "./pages/EstateDetails.jsx";
+import EstateDetails from "./Pages/estate-details.jsx";
 import Footer from "./components/Footer.jsx";
 import SignupPage from "./Pages/signup.jsx";
-import Login from "./Pages/Login.jsx";
+import LoginPage from "./Pages/login.jsx";
 const App = () => {
   return (
     <div>
@@ -14,7 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Home/:id" element={<EstateDetails />} />
         <Route path="/Signup" element={<SignupPage />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/Login" element={<LoginPage />} />
       </Routes>
       <Footer />
     </div>
